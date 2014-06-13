2cbc1f24dfe0fbe99cf02ba472e620c69eb0b7a7
Than Nguyen <than.nguyen@rackspace.com>
2014-06-13 08:26:10 -0500

    upgrade nodejs

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
 ### Setup NodeJS and NPM
-node.set[:nodejs][:version] = "0.10.21"
-node.set[:nodejs][:checksum] = "7c125bf22c1756064f2a68310d4822f77c8134ce178b2faa6155671a8124140d"
-node.set[:nodejs][:checksum_linux_x86] = "c0ed641961a5c5a4602b1316c3d3ed12b3ac330cc18abf3fb980f0b897b5b96b"
-node.set[:nodejs][:checksum_linux_x64] = "2791efef0a1e9a9231b937e55e5b783146e23291bca59a65092f8340eb7c87c8"
+node.set[:nodejs][:version] = "0.10.28"
+node.set[:nodejs][:checksum] = "abddc6441e0f208f6ed8a045e0293f713ea7f6dfb2d6a9a2024bf8b1b4617710"
+node.set[:nodejs][:checksum_linux_x86] = "81ee7f30c35e1743790fd9ca47235bdec4a6c9d2b89a70f33c69e80008cbf422"
+node.set[:nodejs][:checksum_linux_x64] = "5f41f4a90861bddaea92addc5dfba5357de40962031c2281b1683277a0f75932"
 
 include_recipe "nodejs::install_from_binary"
 
